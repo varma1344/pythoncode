b64878cd2feca1cdd2eb15a3bf7cd6360078087d
varma1344 <114913522+varma1344@users.noreply.github.com>
2022-10-05 15:06:23 +0530

    Delete varma.py
    n

--- varma.py
+++
@@ -1,8 +0,0 @@
-def myMax(list1):
-    max = list1[0]
-    for x in list1:
-        if x > max:
-            max = x
-    return max
-list1 = list(map(int,input().Split()))
-print("Largest element is:myMax(list)")
